002353d848e99b0479f18ed884d181f8912cd651
Atish Kumar Kushwaha <62862770+atishbhu@users.noreply.github.com>
2024-07-05 20:20:55 +0530

    Create infinitecurrying.js
    currying with infinite implemetation

---
+++ infinitecurrying.js
@@ -0,0 +1,11 @@
+function add (a) {
+    return function(b) {
+        if(b) {
+            return add(a+b);
+        }else {
+            return a;
+        }
+    }
+}
+
+console.log(add(2)(3)(5)(25)(50)(100)())
